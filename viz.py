1d67a114ad4887e3e3959a7fc0c9e147935d7573
rdslade <rdslade@umich.edu>
2018-08-24 09:49:57 -0500

    support for multiple devices

--- viz.py
+++ viz.py
@@ -75,7 +75,9 @@ class Station:
             messagebox.showinfo("Serial Error", "Cannot open " + self.com + "\nPlease kill and restart the program")
             exit(1);
         self.mac = self.ipa = ""
-        self.programIPA = "172.20.206.8" + str(stat_num)
+        endOfIPA = 80 + stat_num;
+        self.programIPA = "172.20.206." + str(endOfIPA)
+        print(self.programIPA)
         self.thread = threading.Thread(target = self.process)
         self.frame = tk.Frame(parent_)
         self.initComponents()
